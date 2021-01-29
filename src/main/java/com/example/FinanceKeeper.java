2e8fc0e4a93abc11b963e52694e737cd0c756a6a
XLUNIX <alex-shtunder@yandex.ru>
2021-01-29 21:35:26 +0300

    Initial commit #2

--- src/main/java/com/example/FinanceKeeper.java
+++ src/main/java/com/example/FinanceKeeper.java
@@ -1,4 +1,22 @@
 package com.example;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class FinanceKeeper {
+    public static void main(String[] args) {
+        init();
+    }
+
+    private static void init() {
+        try {
+            GraphicsEnvironment graphEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            graphEnv.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/roboto/Roboto-Light.ttf")));
+        } catch (FontFormatException | IOException e) {
+            Logger.getLogger(FinanceKeeper.class.getName()).log(Level.SEVERE, "Problems with fonts...", e);
+        }
+    }
 }
--- src/main/java/com/example/constants/Constants.java
+++ src/main/java/com/example/constants/Constants.java
@@ -1,4 +1,28 @@
 package com.example.constants;
 
-public class Constants {
+import java.util.HashMap;
+
+public final class Constants {
+    public static final String PROGRAM_NAME = "Домашняя бухгалтерия";
+
+    // Menu items
+    public static final String MENU_FILE = "Файл";
+    public static final String MENU_EDIT = "Правка";
+    public static final String MENU_VIEW = "Вид";
+    public static final String MENU_HELP = "Помощь";
+
+    // Months
+    public static final String JANUARY = "Январь";
+    public static final String FEBRUARY = "Февраль";
+    public static final String MARCH = "Март";
+    public static final String APRIL = "Апрель";
+    public static final String MAY = "Май";
+    public static final String JUNE = "Июнь";
+    public static final String JULY = "Июль";
+    public static final String AUGUST = "Август";
+    public static final String SEPTEMBER = "Сентябрь";
+    public static final String OCTOBER = "Октябрь";
+    public static final String NOVEMBER = "Ноябрь";
+    public static final String DECEMBER = "Декабрь";
+
 }
