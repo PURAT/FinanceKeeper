43b99a07bd2e88e5152cad5121401e8ecd2b0145
XLUNIX <alex-shtunder@yandex.ru>
2021-01-30 21:18:58 +0300

    Added Account, Transaction and Transfer classes.

---
+++ src/main/java/com/example/model/Transaction.java
@@ -0,0 +1,100 @@
+package com.example.model;
+
+import com.example.exception.ModelException;
+import javafx.scene.shape.Arc;
+
+import java.util.Date;
+
+public class Transaction extends Common {
+
+    private Account account;
+    private Article article;
+    private double amount;
+    private String notice;
+    private Date date;
+
+    public Transaction(Account account, Article article, double amount) throws ModelException {
+        this(account, article, amount, "", new Date());
+    }
+
+    public Transaction(Account account, Article article, double amount, Date date) throws ModelException {
+        this(account, article, amount, "", date);
+    }
+
+    public Transaction(Account account, Article article, double amount, String notice) throws ModelException {
+        this(account, article, amount, notice, new Date());
+    }
+
+    public Transaction(Account account, Article article, double amount, String notice, Date date) throws ModelException {
+        super();
+        this.account = account;
+        this.article = article;
+        this.amount = amount;
+        this.notice = notice;
+        this.date = date;
+    }
+
+    @Override
+    public String getValueForBox() {
+        return null;
+    }
+
+    @Override
+    public void checkOnException() throws ModelException {
+        if (article == null)
+            throw new ModelException(ModelException.ARTICLE_EMPTY);
+        if (account == null)
+            throw new ModelException(ModelException.ACCOUNT_EMPTY);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "account=" + account +
+                ", article=" + article +
+                ", amount=" + amount +
+                ", notice='" + notice + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
