a5fdc8c0406e59541b646f9fa8132a96c5b46083
XLUNIX <alex-shtunder@yandex.ru>
2021-03-25 21:40:00 +0300

    Added abstract TableModel

---
+++ src/main/java/com/example/gui/table/model/TableModel.java
@@ -0,0 +1,52 @@
+package com.example.gui.table.model;
+
+import com.example.gui.Refresh;
+import com.example.model.Common;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class TableModel extends AbstractTableModel implements Refresh {
+
+    protected List<? extends Common> data;
+    protected List<String> columns = new ArrayList<>();
+
+    public TableModel(List<? extends Common> data) {
+        this.data = data;
+    }
+
+    @Override
+    public void refresh() {
+        updateData();
+        fireTableDataChanged();
+        fireTableStructureChanged();
+    }
+
+    protected abstract void updateData();
+
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.size();
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columns.get(columnIndex);
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        Object object = getValueAt(0, columnIndex);
+
+        if (object == null)
+            return Object.class;
+
+        return object.getClass();
+    }
+}
